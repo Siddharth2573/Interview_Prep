8a1941bb6a7ab054ec16ad5ffceef6781caf3939
Siddharth Yadav <siddharth.yadav2072316@gmail.com>
2026-01-17 11:52:15 +0530

    Update Maximum_Subarray.py

--- DSA/Arrays/Easy/Maximum_Subarray.py
+++ DSA/Arrays/Easy/Maximum_Subarray.py
@@ -1,1 +1,16 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum=nums[0]
+        curr_sum=0
+        for i in range(len(nums)):
+            if curr_sum < 0:
+                curr_sum = 0
+            curr_sum += nums[i]
+            max_sum = max(max_sum,curr_sum)
+        return max_sum
 
+# Kadane's algorithm
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+# https://leetcode.com/problems/maximum-subarray/
